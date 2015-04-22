2f5ec9b328a2a34b99a1952479915a7f9219ec37
NoelMacwan <yulespencer@gmail.com>
2015-04-22 23:05:27 +0530

    Tesla Bringup
    Signed-off-by: NoelMacwan <yulespencer@gmail.com>

--- tesla.mk
+++ tesla.mk
@@ -31,13 +31,13 @@ $(call inherit-product, device/sony/tianchi/aosp_d5303.mk)
 # Inherit from common resources
 $(call inherit-product, device/sony/common/resources.mk)
 
-# Inherit CM common stuff.
-$(call inherit-product, vendor/cm/config/common_full_phone.mk)
+# Inherit Tesla common stuff.
+$(call inherit-product, vendor/tesla/config/common_full_phone.mk)
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
     PRODUCT_NAME=D5303 \
     BUILD_FINGERPRINT=Sony/D5303/D5303:4.4.3/19.1.1.A.0.165/Wv93Zw:user/release-keys \
     PRIVATE_BUILD_DESC="D5303-user 4.4.3 19.1.1.A.0.165 Wv93Zw release-keys"
 
-PRODUCT_NAME := cm_tianchi
+PRODUCT_NAME := tesla_tianchi
 PRODUCT_DEVICE := tianchi
